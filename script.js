3cedc222526cc5982927c00f9bb436b79bdad9aa
Rango1996 <fahimmannan45@gmail.com>
2023-07-02 19:43:18 +0100

    initial configuration

---
+++ script.js
@@ -0,0 +1,8 @@
+const addBtn = document.getElementById("addBook");
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+
+addBtn.addEventListener('click', function() {
+
+})
